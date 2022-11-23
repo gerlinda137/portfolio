aeb7ceda59c32cfecdd79532dcd532f33346a715
Angelina Baranova <angelinabar52@gmail.com>
2022-11-23 22:02:47 +0400

    styled header and top section

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,29 +1,38 @@
-const gulp = require('gulp')
-const plumber = require('gulp-plumber')
-const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css')
-const sourcemaps = require('gulp-sourcemaps')
-const shorthand = require('gulp-shorthand')
-const autoprefixer = require('gulp-autoprefixer')
-const rename = require("gulp-rename")
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const shorthand = require("gulp-shorthand");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
 
 module.exports = function styles() {
-  return gulp.src('src/styles/*.scss')
+  return gulp
+    .src("src/styles/**/*.scss")
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(autoprefixer({
-      cascade: false
-    }))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
     .pipe(shorthand())
-    .pipe(cleanCSS({
-      debug: true,
-      compatibility: '*'
-    }, details => {
-      console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`)
-    }))
+    .pipe(
+      cleanCSS(
+        {
+          debug: true,
+          compatibility: "*",
+        },
+        (details) => {
+          console.log(
+            `${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`
+          );
+        }
+      )
+    )
     .pipe(sourcemaps.write())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('build/css'))
-}
-
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("build/css"));
+};
