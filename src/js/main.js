221d7e51d66b8dcbf9168681be2f6ef50c060b46
Angelina Baranova <angelinabar52@gmail.com>
2022-11-28 20:27:57 +0400

    finished gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,33 @@
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
+// import Swiper from "swiper";
+// import "swiper/swiper-bundle.css";
 
-import 'normalize.css';
+import "normalize.css";
 
-const gbgbug = new Swiper();
-gbgbug.nygbugb();
+// const gbgbug = new Swiper();
+// gbgbug.nygbugb();
 
+const portfolio = document.querySelector(`.portfolio`);
+const portfolioCards = portfolio.querySelectorAll(`.project-card`);
+let portfolioCardsHidden = [];
+const showMoreBtn = portfolio.querySelector("#show-more");
+
+for (let i = 4; i < portfolioCards.length; i++) {
+  portfolioCards[i].classList.add("hidden");
+  portfolioCardsHidden.push(portfolioCards[i]);
+}
+
+showMoreBtn.addEventListener("click", () => {
+  if (showMoreBtn.dataset.shown == "false") {
+    for (const card of portfolioCardsHidden) {
+      card.classList.remove("hidden");
+      showMoreBtn.textContent = "Скрыть";
+      showMoreBtn.dataset.shown = "true";
+    }
+  } else {
+    for (const card of portfolioCardsHidden) {
+      card.classList.add("hidden");
+      showMoreBtn.textContent = "Показать больше";
+      showMoreBtn.dataset.shown = "false";
+    }
+  }
+});
