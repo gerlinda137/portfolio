9c96fdadea4971397e125671b9b5e2a528076dae
Angelina Baranova <angelinabar52@gmail.com>
2022-11-30 14:15:04 +0400

    education section

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,22 @@
-// import Swiper from "swiper";
-// import "swiper/swiper-bundle.css";
+import Swiper from "swiper";
+import "swiper/swiper-bundle.css";
 
 import "normalize.css";
 
-// const gbgbug = new Swiper();
-// gbgbug.nygbugb();
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  spaceBetween: 38,
+  slidesPerView: "auto",
+  // centeredSlides: true,
+  // loop: true,
+  slidesOffsetBefore: 102,
+  slidesOffsetAfter: 20,
+});
 
 const portfolio = document.querySelector(`.portfolio`);
 const portfolioCards = portfolio.querySelectorAll(`.project-card`);
-let portfolioCardsHidden = [];
+const portfolioCardsHidden = [];
 const showMoreBtn = portfolio.querySelector("#show-more");
 
 for (let i = 4; i < portfolioCards.length; i++) {
@@ -17,7 +25,7 @@ for (let i = 4; i < portfolioCards.length; i++) {
 }
 
 showMoreBtn.addEventListener("click", () => {
-  if (showMoreBtn.dataset.shown == "false") {
+  if (showMoreBtn.dataset.shown === "false") {
     for (const card of portfolioCardsHidden) {
       card.classList.remove("hidden");
       showMoreBtn.textContent = "Скрыть";
